611213f8c32bf3415a30b2994db5771c8a912443
E1FANG <438283787@qq.com>
2021-06-14 00:45:07 +0800

    只在生产环境提取CSS文件;

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,29 @@
-const ESLintPlugin = require('eslint-webpack-plugin')
-const path = require('path')
+const ESLintPlugin = require('eslint-webpack-plugin');
+const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const mode = 'production';
 
-const cssLoaders = (...loaders)=>[
-  // 'style-loader',
-  MiniCssExtractPlugin.loader,
-    {
-      loader:'css-loader',
-      options: {
-        modules: {
-          compileType: 'icss',
-        }
+const cssLoaders = (...loaders) => [
+  mode === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
+  {
+    loader: 'css-loader',
+    options: {
+      modules: {
+        compileType: 'icss',
       }
-    },
+    }
+  },
   ...loaders
-]
+];
 
 module.exports = {
-  mode:'production',
+  mode,
   plugins: [
     new ESLintPlugin({
-      extensions:['.js','.jsx','ts','tsx'] // 不加.jsx就不会检查jsx文件了
+      extensions: ['.js', '.jsx', 'ts', 'tsx'] // 不加.jsx就不会检查jsx文件了
     }),
     new MiniCssExtractPlugin({
-      filename:'[name].[contenthash].css'
+      filename: '[name].[contenthash].css'
     })
   ],
   output: {
@@ -31,7 +31,7 @@ module.exports = {
   },
   resolve: {
     alias: {
-      'src':path.resolve(__dirname,'./src/')
+      'src': path.resolve(__dirname, './src/')
     }
   },
   module: {
@@ -42,9 +42,9 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets:[
+            presets: [
               ['@babel/preset-env'],
-              ['@babel/preset-react',{runtime:'classic'}],
+              ['@babel/preset-react', {runtime: 'classic'}],
               ['@babel/preset-typescript']
             ]
           }
@@ -52,8 +52,8 @@ module.exports = {
       },
       {
         test: /\.less$/i,
-        use:cssLoaders({
-          loader:'less-loader',
+        use: cssLoaders({
+          loader: 'less-loader',
           options: {
             additionalData: `
               @import "~src/less-vars.less";
@@ -63,18 +63,18 @@ module.exports = {
       },
       {
         test: /\.s[ac]ss$/i,
-        use:cssLoaders({
+        use: cssLoaders({
           loader: 'sass-loader',
           options: {
-            additionalData:`
+            additionalData: `
               @import "src/scss-vars.scss";
               `,
-            sassOptions:{
-              includePaths:[__dirname]
+            sassOptions: {
+              includePaths: [__dirname]
             }
           }
         })
       }
     ]
   }
-}
+};

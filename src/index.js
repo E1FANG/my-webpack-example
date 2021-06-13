5f6c73a5d76591d624259d59027c56f4a1458f6f
E1FANG <438283787@qq.com>
2021-06-13 13:27:10 +0800

    使用babel-loader打包JS

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
+import {a} from './a'
+import {b} from './b'
+
+console.log(a+b)
+
 const str = 'hi'
 console.log(str)
+
+const fn1 = ()=>{
+  console.log('fn1')
+}
+
+console.log(Promise.resolve("test promise"));
